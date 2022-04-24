e0c3c7d15158fe4f7735f0f3f710048b1a313ad4
generic-github-user <40661852+generic-github-user@users.noreply.github.com>
2022-04-24 18:42:41 -0400

    Add some more sample code, etc

--- parser.py
+++ parser.py
@@ -1,2 +1,38 @@
 import string
 sample = '1::10**4 $ !+(2:x^.5 ; !x%y) join\n'
+sample2 = ''
+sample3 = """
+# levels of syntactic abstraction?
+constants: null, Infinity
+
+# more general form?
+recrel
+
+func range = a, b -> n in N | a <= n <= b
+func range = a, b ->
+
+func reduce =
+func compose = f
+
+# analogies?
+
+func min = xx -> reduce(xx, a,b -> b<a?b:a)
+func max = xx -> reduce(xx, a,b -> b<a?b:a)
+# func invert
+
+func sum = x -> reduce( '+)
+func mean = x -> sum(x) / x.length
+
+func abs
+
+set(func) summary = (min, max, sum, mean)
+
+# class string = (
+#
+# )
+
+func for = a,b,c -> a; while(b, c)
+"""
+sample4 = """
+x+y
+"""
